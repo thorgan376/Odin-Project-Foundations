63ffead9c7436869fa8ec6ccf5c3ab02de48f4b9
LaptopAcer <76546590+thorgan376@users.noreply.github.com>
2023-10-01 00:44:51 +0700

    learning Javascript exhaustive

--- javascript-foundations/script.js
+++ javascript-foundations/script.js
@@ -608,3 +608,29 @@ String.raw():
 .indexOf(): 
 .isWellFormed()
 ( And a lot of them to play with )*/
+
+
+//////Funstufff/////////
+//Note: All comparison operators return a boolean value
+//true or false
+
+//String are comapared letter by letter
+/*
+The algorithm to compare two strings is simple:
+
+   1. Compare the first character of both strings.
+   2. If the first character from the first string is greater (or less) than the other string’s, then the first string is greater (or less) than the second. We’re done.
+   3. Otherwise, if both strings’ first characters are the same, compare the second characters the same way.
+   4. Repeat until the end of either string.
+   5. If both strings end at the same length, then they are equal. Otherwise, the longer string is greater.
+*/
+//Ex: Glo vs Glee: 
+/*
+1. G == G
+2. l == l
+3. o > e => Stop here 
+Even Glee has more character than Glo, Glo still greater than Glee
+The first the greater*/
+
+//Remmember: In case sensitive: lower case is greater than Uppercase
+/* Because the lower case character has a greater index in the internal encoding table Javascript use (Unicode) */
