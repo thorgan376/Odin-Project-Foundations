6106cd79b3322a7586360ee3ed9b5e992ce7ad13
LaptopAcer <76546590+thorgan376@users.noreply.github.com>
2023-10-05 12:45:26 +0700

    JAVASCRIPT SWICH STATEMENT In this part, i continue learning about conditional switch: 1. How can switch works (Switching details) 2. What is the break key words 3. Strict comparison

--- javascript-foundations/FDM-2-conditionals.js
+++ javascript-foundations/FDM-2-conditionals.js
@@ -175,7 +175,7 @@ console.log(`null === +"\n0\n" : ${null === +"\n0\n"}`);
 console.log("   JAVASCRIPT IF, ELSE, AND ELSE IF");
 //Conditional statements are used to perform different actions based on different conditions.
 
-console.log("     Conditional Statements :");
+console.log("     Conditional Statements : (Look in the code for more info)");
 /*
 - Use IF to specify a block of code to be executed, if a specified condition is true
 - Use ELSE to specify a block of code to be executed, if the same condition is false
@@ -200,4 +200,119 @@ else {
   console.log("Good evening");
 } 
 
+console.log("   JAVASCRIPT SWICH STATEMENT: (Look in the code for more info)")
+//The switch statement is used to perform different actions based on different conditions.
+//Use the switch statement to select one of many code blocks to be executed.
 
+/*This is how it works:
+
+    The switch expression is evaluated once.
+    The value of the expression is compared with the values of each case.
+    If there is a match, the associated block of code is executed.
+    If there is no match, the default code block is executed.
+*/
+
+console.log("     SWITCH EXAMPLE 1 :");
+switch(new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    console.log(`Today is: ${day}`);
+    break;
+  case 1:
+    day = "Monday";
+    console.log(`Today is: ${day}`);
+    break;
+  case 2:
+     day = "Tuesday";
+     console.log(`Today is: ${day}`);
+    break;
+  case 3:
+    day = "Wednesday";
+    console.log(`Today is: ${day}`);
+    break;
+  case 4:
+    day = "Thursday";
+    console.log(`Today is: ${day}`);
+    break;
+  case 5:
+    day = "Friday";
+    console.log(`Today is: ${day}`);
+    break;
+  case 6:
+    day = "Saturday";
+    console.log(`Today is: ${day}`);
+    break;
+  default:
+    day = "This day is not exsist, how can you come here ?"
+    console.log(`Today is: ${day}`);
+    break
+}
+console.log("     THE BREAK KEY WORD : (Look in the code for more info)");
+/*
+- When JavaScript reaches a break keyword, it breaks out of the switch block.
+
+- This will stop the execution inside the switch block.
+
+- It is not necessary to break the last case in a switch block. The block breaks (ends) there anyway. 
+*/
+
+console.log("     THE DEFAULT KEY WORD : (Look in the code for more info)");
+//The default keyword specifies the code to run if there is no case match:
+
+console.log("   COMMON CODE BLOCKS :");
+console.log("     SWITCH EXAMPLE 2 : ");
+switch (a = new Date().getDay()) {
+  case 4:
+  case 5:
+    //console.log(a);
+    text = "Soon it is Weekend";
+    if (a == 4) {
+      a = "Thursday";
+    } else { //a ==5
+      a = "Friday";
+    }
+    console.log(`Hmm, today is : ${a} and ${text} `);
+    break;
+  case 0:
+  case 6:
+    text = "It is Weekend";
+    if (a == 0) {
+      a = "Sunday";
+    } else {
+      a = "Saturday";
+    }
+    console.log(`Hmm, today is : ${a} and ${text} `);
+    break;
+  default:
+    text = "Looking forward to the Weekend";
+}
+
+console.log("     SWITCHING DETAILS : (Look in the code for more info)");
+/*
+If multiple cases matches a case value, the first case is selected.
+
+If no matching cases are found, the program continues to the default label.
+
+If no default label is found, the program continues to the statement(s) after the switch. */
+
+
+console.log("     STRICT COMPARION : (Look in the code too understand why)");
+//switch case use strict comparison (===).
+// Operands must be same type and value to be match.
+// In this example there will be no match for x: 
+console.log("A not-working example :");
+let x = "0";
+switch (x) {
+  case 0:
+    text = "Off";
+    console.log(text);
+    break;
+  case 1:
+    text = "On";
+    console.log(text);
+    break;
+  default:
+    text = "No value found";
+    console.log(text);
+}
+//END OF SWITCH EXAMPLE`
